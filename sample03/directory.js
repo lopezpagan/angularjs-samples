6a62e9233d2e13445544137da15d2db883417e49
Tony <lopezpagan@gmail.com>
2015-08-27 19:03:33 -0400

    Search and filters

---
+++ sample03/directory.js
@@ -0,0 +1,18 @@
+angular.module('directoryApp', [])
+       .controller('directoryCtrl', function(){
+
+         var dirList = this;
+
+          dirList.list = [
+            {name: 'Tony', age: 40},
+            {name: 'Jose', age: 35},
+            {name:'Chuito', age: 30}
+          ];
+
+          dirList.addPerson = function() {
+
+              dirList.list.push({name: dirList.name, age: dirList.age});
+              dirList.name = '';
+              dirList.age = 0;
+          };
+       });
